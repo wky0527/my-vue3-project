00285f24933929eec7b1af3915d66c758ba6a34b
wky0527 <805245271@qq.com>
2022-04-26 16:40:50 +0800

    feat: uni-modules上传

---
+++ uniCloud-aliyun/cloudfunctions/logout/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const db = uniCloud.database()
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	const res =db.collection('logout').get()	
+	//返回数据给客户端
+	return res
+};
